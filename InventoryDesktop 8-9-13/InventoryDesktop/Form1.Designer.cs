dd41a6ceda2e4e84ebc33b6f5e56b372908bce1e
richk1693 <richk1693@gmail.com>
2015-02-23 16:29:24 -0500

    2/23/15 initial push
    Initial push of the AMP Inventory Desktop version

---
+++ InventoryDesktop 8-9-13/InventoryDesktop/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace InventoryDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.table2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.appDatabase2DataSet = new InventoryDesktop.AppDatabase2DataSet();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.table2TableAdapter = new InventoryDesktop.AppDatabase2DataSetTableAdapters.Table2TableAdapter();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appDatabase2DataSet)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(700, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(658, 260);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // table2BindingSource
+            // 
+            this.table2BindingSource.DataMember = "Table2";
+            this.table2BindingSource.DataSource = this.appDatabase2DataSet;
+            // 
+            // appDatabase2DataSet
+            // 
+            this.appDatabase2DataSet.DataSetName = "AppDatabase2DataSet";
+            this.appDatabase2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(700, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, -3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(795, 343);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.deleteButton);
+            this.tabPage1.Controls.Add(this.addButton);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(787, 317);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Database";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(699, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Year End";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(700, 156);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(700, 185);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 5;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(787, 317);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Reports";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(706, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Print";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(706, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Reorder";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(7, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(504, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // table2TableAdapter
+            // 
+            this.table2TableAdapter.ClearBeforeFill = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 294);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.appDatabase2DataSet)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox1;
+        private AppDatabase2DataSet appDatabase2DataSet;
+        private System.Windows.Forms.BindingSource table2BindingSource;
+        private InventoryDesktop.AppDatabase2DataSetTableAdapters.Table2TableAdapter table2TableAdapter;
+        private System.Windows.Forms.Button button5;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ InventoryDesktop 8-9-13/InventoryDesktop/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+using System.Configuration;
+using System.IO;
+
+namespace InventoryDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private StringReader myReader;
+        SqlCeConnection conn = new SqlCeConnection();
+        //SqlCeConnection conn = new SqlCeConnection(@"Data Source = C:\Users\Dawg\Documents\Visual Studio 2008\Projects\InventoryDesktop\InventoryDesktop\AppDatabase2.sdf");
+        //SqlCeConnection conn = new SqlCeConnection(@"Data Source = E:\Documents and Settings\Larry\My Documents\WINDOWSMOBILE15 My Documents\AppDatabase2.sdf");
+        //SqlCeConnection conn = new SqlCeConnection(@"Data Source = C:\Users\Larry\Documents\Documents on SS 1\AppDatabase2.sdf");
+
+
+        //All of the on load events weren't made by me so i'm scared to mess with them.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*// TODO: This line of code loads data into the 'appDatabase2DataSet.Table2' table. You can move, or remove it, as needed.
+            this.table2TableAdapter.Fill(this.appDatabase2DataSet.Table2);
+            // TODO: This line of code loads data into the 'appDatabase2DataSet.Table2' table. You can move, or remove it, as needed.
+            this.table2TableAdapter.Fill(this.appDatabase2DataSet.Table2);
+            // TODO: This line of code loads data into the 'appDatabase2DataSet.Table2' table. You can move, or remove it, as needed.
+            this.table2TableAdapter.Fill(this.appDatabase2DataSet.Table2);
+            // TODO: This line of code loads data into the 'appDatabase2DataSet.Table2' table. You can move, or remove it, as needed.
+            this.table2TableAdapter.Fill(this.appDatabase2DataSet.Table2);
+             */
+            if(File.Exists(@"C:\Users\Dawg\Documents\Visual Studio 2008\Projects\InventoryDesktop\InventoryDesktop\AppDatabase2.sdf"))
+                conn.ConnectionString = @"Data Source = C:\Users\Dawg\Documents\Visual Studio 2008\Projects\InventoryDesktop\InventoryDesktop\AppDatabase2.sdf";
+            if (File.Exists(@"C:\Users\clem\My Documents\Documents on SS 1\AppDatabase2.sdf"))
+                conn.ConnectionString = @"Data Source = C:\Users\clem\My Documents\Documents on SS 1\AppDatabase2.sdf";
+            if (File.Exists(@"C:\Users\Larry\Documents\Documents on SS 1\AppDatabase2.sdf"))
+                conn.ConnectionString = @"Data Source = C:\Users\Larry\Documents\Documents on SS 1\AppDatabase2.sdf";
+            reloadGrid();
+        }
+
+
+        //Attempt to reload the grid.
+        private void reloadGrid() {
+            SqlCeDataAdapter dataAdapter = new SqlCeDataAdapter("SELECT * FROM Table2", conn);
+            DataTable table = new DataTable();
+            dataAdapter.Fill(table);
+            table.DefaultView.Sort="Type,barcode";
+            dataGridView1.DataSource = table;
+            
+            //view.Sort= "Barcode, min";
+            //dataGridView1.DataSource = view;
+
+        }
+        //Auto-generated method. I'm not touchin it.
+        private void ToCsV(DataGridView dGV, string filename)
+        {
+            string stOutput = "";
+            // Export titles:
+            string sHeaders = "";
+
+            for (int j = 0; j < dGV.Columns.Count; j++)
+                sHeaders = sHeaders.ToString() + Convert.ToString(dGV.Columns[j].HeaderText) + "\t";
+            stOutput += sHeaders + "\r\n";
+            // Export data.
+            for (int i = 0; i < dGV.RowCount - 1; i++)
+            {
+                string stLine = "";
+                for (int j = 0; j < dGV.Rows[i].Cells.Count; j++)
+                    stLine = stLine.ToString() + Convert.ToString(dGV.Rows[i].Cells[j].Value) + "\t";
+                stOutput += stLine + "\r\n";
+            }
+            Encoding utf16 = Encoding.GetEncoding(1254);
+            byte[] output = utf16.GetBytes(stOutput);
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(output, 0, output.Length); //write the encoded file
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+        }
+
+       
+        //Export the database as an Excel file on button click.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "export.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //ToCsV(dataGridView1, @"c:\export.xls");
+                ToCsV(dataGridView1, sfd.FileName); // Here dataGridview1 is your grid view name 
+            }  
+        }
+
+
+        //Save changes in the current row to the database
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            
+            string test = dataGridView1[1, dataGridView1.CurrentCell.RowIndex].Value.ToString();
+            //Giant query is just an update for each database value
+            string query = @"UPDATE Table2 SET onHand = @1, madeYTD = @2, soldYTD = @3, Min = @4,
+                            EOQ = @5, ytdMoAvg = @6, Cost = @7, Value = @8, type = @9 WHERE [Barcode] = (@0);";
+            conn.Open();
+            SqlCeCommand cmd = new SqlCeCommand(query, conn);
+            //Each line is a column from the dataGridView, establishing the relationship between them and the db.
+            cmd.Parameters.AddWithValue("@0", dataGridView1[0,dataGridView1.CurrentCell.RowIndex].Value.ToString());
+            cmd.Parameters.AddWithValue("@1", dataGridView1[1, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@2", dataGridView1[2, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@3", dataGridView1[3, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@4", dataGridView1[4, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@5", dataGridView1[5, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@6", dataGridView1[6, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@7", dataGridView1[7, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@8", dataGridView1[8, dataGridView1.CurrentCell.RowIndex].Value);
+            cmd.Parameters.AddWithValue("@9", dataGridView1[10, dataGridView1.CurrentCell.RowIndex].Value.ToString());
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Save Successful");
+        }
+
+
+        //Fill listBox1 with reorder information when "Reorder" button is clicked.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlCeCommand cmd = new SqlCeCommand("SELECT * FROM Table2", conn);
+            conn.Open();
+            SqlCeDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                int a = rdr.GetInt32(1);
+                int b = rdr.GetInt32(4);
+
+                //If amount on hand is <= reorder then add it to the reorder list.
+                if (rdr.GetInt32(1) <= rdr.GetInt32(4))
+                {
+                    listBox1.Items.Add(rdr.GetString(0) +"           On Hand:"+ rdr.GetInt32(1).ToString() + "           Reorder: " + rdr.GetInt32(4).ToString() + "           EOQ: " + rdr.GetInt32(5));
+                }
+
+            }
+            conn.Close();
+        }
+
+
+
+        //Function to print listBox from online tutorial. I'm assuming it's magic cuz i dont understand it.
+        protected void printDocument1_PrintPage(object sender,System.Drawing.Printing.PrintPageEventArgs ev)
+  {
+      float linesPerPage = 0;
+      float yPosition = 0;
+      int count = 0;
+      float leftMargin = ev.MarginBounds.Left;
+      float topMargin = ev.MarginBounds.Top;
+      string line = null;
+     //Font printFont = this.listBox1.Font;
+       Font printFont = new Font("times New Roman", 12);
+     SolidBrush myBrush = new SolidBrush(Color.Black);
+ 
+     // Work out the number of lines per page, using the MarginBounds.
+      linesPerPage = ev.MarginBounds.Height / printFont.GetHeight(ev.Graphics);
+ 
+     // Iterate over the string using the StringReader, printing each line.
+      while (count < linesPerPage && ((line = myReader.ReadLine()) != null))
+     {
+         // calculate the next line position based on
+         // the height of the font according to the printing device
+          yPosition = topMargin + (count * printFont.GetHeight(ev.Graphics));
+ 
+         // draw the next line in the rich edit control
+  
+         ev.Graphics.DrawString(line, printFont,
+                                myBrush, leftMargin,
+                                yPosition, new StringFormat());
+         count++;
+     }
+      
+ 
+     // If there are more lines, print another page.
+     if (line != null)
+         ev.HasMorePages = true;
+     else
+         ev.HasMorePages = false;
+ 
+     myBrush.Dispose();
+ }
+        
+        //When the print button is clicked, print the document! (Online tutorial)
+        private void button5_Click(object sender, EventArgs e)
+        {
+           printDialog1.Document = printDocument1;
+            string strText = "";
+            foreach (object x in listBox1.Items)
+            {
+                 strText = strText + x.ToString() + "\n";
+            }
+ 
+            myReader = new StringReader(strText);
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+             {
+                this.printDocument1.Print();
+             }
+
+        }
+
+        //Add a record to the database
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            bool flag = true;
+            for (int i = 0; i < 11; i++)
+            {
+                if (dataGridView1[i, dataGridView1.CurrentCell.RowIndex].Value.ToString() == "")
+                {
+                    flag = false;
+                    MessageBox.Show("Please insert a value for every field before adding");
+                    break;
+                }
+            }
+            if (flag)
+            {
+                conn.Open();
+                SqlCeCommand cmd = new SqlCeCommand(@"INSERT INTO Table2 (barcode, onHand, madeYTD, soldYTD,
+                                                min, EOQ, ytdMoAvg, cost, value, onHand1231, type) VALUES (@0,@1,
+                                                @2,@3,@4,@5,@6,@7,@8,@9,@10);", conn);
+                cmd.Parameters.AddWithValue("@0", dataGridView1[0, dataGridView1.CurrentCell.RowIndex].Value.ToString());
+                cmd.Parameters.AddWithValue("@1", dataGridView1[1, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@2", dataGridView1[2, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@3", dataGridView1[3, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@4", dataGridView1[4, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@5", dataGridView1[5, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@6", dataGridView1[6, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@7", dataGridView1[7, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@8", dataGridView1[8, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@9", dataGridView1[9, dataGridView1.CurrentCell.RowIndex].Value);
+                cmd.Parameters.AddWithValue("@10", dataGridView1[10, dataGridView1.CurrentCell.RowIndex].Value.ToString());
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        //Remove a record from the database
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Are you sure you wish to delete row " + dataGridView1[0,dataGridView1.CurrentCell.RowIndex].Value.ToString(), "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                conn.Open();
+                SqlCeCommand cmd = new SqlCeCommand("DELETE FROM Table2 WHERE [barcode] = @0", conn);
+                cmd.Parameters.AddWithValue("@0", dataGridView1[0, dataGridView1.CurrentCell.RowIndex].Value.ToString());
+                cmd.ExecuteNonQuery();
+                reloadGrid();
+                conn.Close();
+                MessageBox.Show("Record Deleted");
+            }
+            
+        }
+
+        //End of year changes. made and sold YTD = 0. 1231 = current onhand.
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var result = MessageBox.Show("Are you sure you wish to run end of year?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                conn.Open();
+                SqlCeCommand cmd = new SqlCeCommand("SELECT * FROM Table2", conn);
+                SqlCeCommand cmd2 = new SqlCeCommand("UPDATE Table2 SET madeYTD = 0, soldYTD = 0, onHand1231 = @1 WHERE barcode = @2", conn);
+                SqlCeDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    cmd2.Parameters.AddWithValue("@1", rdr.GetInt32(1).ToString());
+                    cmd2.Parameters.AddWithValue("@2", rdr.GetString(0));
+                    cmd2.ExecuteNonQuery();
+                    cmd2.Parameters.Clear();
+                }
+                reloadGrid();
+                conn.Close();
+                MessageBox.Show("End of year executed successfully.");
+            }
+        }
+
+
+
+        
+    }
+}
